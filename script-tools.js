56343a9505e23284c3c082904b5412ecb486902a
Nick Charbonneau <nick.charbonneau@azul7.com>
2014-02-13 14:42:52 -0600

    Added support for multiple parallel loads

--- script-tools.js
+++ script-tools.js
@@ -3,30 +3,54 @@
    Hacker: Nick Charbonneau
 
    Needed a script loader.
-   load: appends script to the body and exicutes a callback (if provided)
+   load: appends script (single string or array) paths to the body and executes a callback (if provided) on load 
    executeFunctionByName: executes function by name(string), context(object), arguments(array)
    isFunction: checks if the given object is a function. 
    ------------------------------------------------------------------------ */
 
 HLIB.ScriptTools = {
     load: function(scriptSrc, callback, callbackContext, arguments) {
-        var script = document.createElement('script'),
-            self = this;
-        script.type = 'text/javascript';
-        script.src = scriptSrc;
-        document.body.appendChild(script);
-        if (callback) {
+        var self = this,
+            loaded = new Array();
+        
+        if(!(scriptSrc instanceof Array )){
+            scriptSrc = new Array(scriptSrc);
+        };
+        for (var i = 0; i < scriptSrc.length; i++) {
+            loaded[i] = addScriptToDOM(scriptSrc[i], i);
+            executeCallback(loaded[i] , i);
+        }
+
+        function addScriptToDOM(scriptSrc, index){
+            var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.src = scriptSrc;
+            document.body.appendChild(script);            
+            return script;
+        }
+        function executeCallback(script, index){
             script.onload=script.onreadystatechange=function(){
-                if (  !this.readyState || this.readyState == 'complete'  ){
-                    var context = callbackContext || window;
-                    if(self.isFunction(callback)){
-                        callback.call(callbackContext);
-                    } else {
-                        self.executeFunctionByName(callback, context, arguments);
+                loaded[index] = true;
+                if(callback && pollReadyStates()){
+                    if (  !this.readyState || this.readyState == 'complete'  ){
+                        var context = callbackContext || window;
+                        if(self.isFunction(callback)){
+                            callback.call(context, arguments);
+                        } else {
+                            self.executeFunctionByName(callback, context, arguments);
+                        }
                     }
                 }
             };
         }
+        function pollReadyStates() {
+            var _ready = true;
+            for( i = 0; i < loaded.length; i++){
+                if (loaded[i] !== true){ _ready = false; }
+            }
+            return _ready;
+        }
+
     },
     executeFunctionByName: function (functionName, context /*, args */) {
         var args = Array.prototype.slice.call(arguments, 2),
